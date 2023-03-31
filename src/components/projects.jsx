402c25804b0e2c5d4aa41d29f5f2dca64cc46d63
jbrogan99 <jack.brogan99@gmail.com>
2023-03-31 15:54:51 +0100

    project styles updated

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,16 +6,28 @@ export const Projects = () => {
     <section className="projects-container">
       <h2>Projects</h2>
       <ul>
-        <li>Recipe</li>
-        <li>Doughnuts</li>
-        <li>Currency Converter</li>
-        <li>Blackjack</li>
-        <li>E-commerce</li>
-        <li>Tip Calculator</li>
+        <li>
+          <span>Recipe</span>
+        </li>
+        <li>
+          <span>Doughnuts</span>
+        </li>
+        <li>
+          <span>Currency Converter</span>
+        </li>
+        <li>
+          <span>Blackjack</span>
+        </li>
+        <li>
+          <span>E-commerce</span>
+        </li>
+        <li>
+          <span>Tip Calculator</span>
+        </li>
       </ul>
-      <a href="github.com/jbrogan99">
+      {/* <a href="github.com/jbrogan99">
         <img alt="github logo" src={github} />
-      </a>
+      </a> */}
     </section>
   );
 };
